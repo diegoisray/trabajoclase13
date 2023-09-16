fc8d823b7945ebf7445cc5029c6b5d6ef0777d3a
CastilloValeria <casvaleli87@gmail.com>
2023-09-16 18:01:19 -0300

    agruege un nuevo archivo txt y modifique vale.json

---
+++ archivonuevo.js
@@ -0,0 +1,8 @@
+let electrodomesticos=["heladera","freezer","cocina","licuadora","lavarropas","procesadora"];
+console.log(electrodomesticos.push());
+
+let el_primero= electrodomesticos.shift
+console.log("es shift1= "+ electrodomesticos);
+
+let dos= electrodomesticos.shift(2)
+console.log("es shift2= "+ electrodomesticos);
